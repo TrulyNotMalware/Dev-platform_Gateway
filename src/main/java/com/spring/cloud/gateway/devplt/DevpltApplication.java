679d431ee9e7cf726b86d4bc89750d46fc4996b7
TrulyNotMalware <notleebutyee@gmail.com>
2022-01-03 21:16:01 +0900

    Initial Commit - Create Template

---
+++ src/main/java/com/spring/cloud/gateway/devplt/DevpltApplication.java
@@ -0,0 +1,14 @@
+package com.spring.cloud.gateway.devplt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DevpltApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DevpltApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/cloud/gateway/devplt/DevpltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.gateway.devplt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevpltApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
