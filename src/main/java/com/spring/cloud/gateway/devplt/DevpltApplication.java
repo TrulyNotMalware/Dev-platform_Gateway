448dcc32daecd0e94e985018f54dbf1cc324df2d
TrulyNotMalware <notleebutyee@gmail.com>
2022-07-22 14:00:20 +0900

    feat : Add Circuit break Pattern - Resilience4j
    - Testing Circuit break pattern
    no related issue

--- src/main/java/com/spring/cloud/gateway/devplt/DevpltApplication.java
+++ src/main/java/com/spring/cloud/gateway/devplt/DevpltApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+//DEPRECATED
+//@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
 public class DevpltApplication {
